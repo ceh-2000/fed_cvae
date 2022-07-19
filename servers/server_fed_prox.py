aca84770d0111a6540379bf3cc7e7410c07e7173
emiliolr <emilio.luz.ricca@gmail.com>
2022-07-19 17:52:03 -0400

    Added a server for FedProx

---
+++ servers/server_fed_prox.py
@@ -0,0 +1,44 @@
+import copy
+
+from servers.server import Server
+from utils import average_weights
+
+
+class ServerFedProx(Server):
+    def __init__(self, base_params):
+        super().__init__(base_params)
+
+    def train(self):
+        """
+        Train the global server model and local user models.
+        After each epoch average the weights of all users.
+        """
+
+        self.user_data_amts = [len(u.dataloader.dataset) for u in self.users]
+
+        for e in range(self.glob_epochs):
+            self.evaluate(e)
+
+            # Sample users for training
+            selected_users = self.sample_users()
+
+            # Replace local user model weights with server model weights for SELECTED users
+            server_model_weights = copy.deepcopy(self.server_model.state_dict())
+            for u in selected_users:
+                u.model.load_state_dict(server_model_weights)
+
+            # Train SELECTED user models
+            for u in selected_users:
+                u.train(self.local_epochs)
+
+            # Save ALL user models to a list
+            models = []
+            for u in self.users:
+                models.append(u.model)
+
+            # Average the weights of ALL user models and save in server
+            state_dict = average_weights(models, data_amts=self.user_data_amts)
+            self.server_model.load_state_dict(copy.deepcopy(state_dict))
+
+            print(f"Finished training {len(selected_users)} users for epoch {e}")
+            print("__________________________________________")
