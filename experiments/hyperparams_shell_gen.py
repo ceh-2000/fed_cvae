1090553e8d956e7946c924be06497c68a98b400c
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-08-10 15:50:15 -0400

    Added in real command

--- experiments/hyperparams_shell_gen.py
+++ experiments/hyperparams_shell_gen.py
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     shell_file_name = f"experiments/hyperparam_runs_{host_num}.sh"
     print(host_num)
 
-    a_vals = [0.1, 0.2, 0.3]
-    b_vals = [1, 2, 3]
-    c_vals = [10, 20, 30]
+    beta_vals = [0.1, 0.5, 1.0]
+    local_lr_vals = [0.01, 0.001, 0.0001]
 
     all_scripts = []
-    for a in a_vals:
-        for b in b_vals:
-            for c in c_vals:
-                all_scripts.append(f"python3 ../main.py -a {a} --b {b} --c {c}")
+    for beta in beta_vals:
+        for local_lr in local_lr_vals:
+            all_scripts.append(
+                f"python3 main.py --algorithm fedvae --dataset mnist --num_users 10 --alpha 1.0 --sample_ratio 0.1 --glob_epochs 5 --local_epochs 5 --should_log 1 --z_dim 50 --beta {beta} --local_LR {local_lr}"
+            )
 
     counter = 0
     cur_host_scripts = []
