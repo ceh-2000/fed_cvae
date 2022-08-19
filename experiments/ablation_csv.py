ce82c5867a77abbef499be2b2a4078cc86608b5e
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-08-19 15:32:28 -0400

    Ablation experiments

--- experiments/ablation_csv.py
+++ experiments/ablation_csv.py
@@ -1,13 +1,11 @@
 import os
 
-import pandas as pd
-
 from experiments.utils import csvs_to_dfs, to_csv
 
 if __name__ == "__main__":
     # Convert ablation results to csvs
     experiment_dir = "../runs"
-    if os.listdir(experiment_dir + "/csv") == None:
+    if not os.path.exists(experiment_dir + "/csv"):
         to_csv(experiment_dir)
 
     # Convert csvs to dataframes
@@ -19,7 +17,16 @@ if __name__ == "__main__":
         df = dfs[df_key]
         df.index.name = "run_name"
 
-        # ablation_exp = dfs[df_key][].str.extract
-        # dfs[df_key] =
+        df["accuracies"] = df[1]
+        df = df.drop(1, axis=1)
+        df["filenames"] = df.index
+        df.index = range(0, len(df))
 
-        # Reassign the cur_df at the end
+        seed_df = df["filenames"].str.extract(r"seed=(?P<seed>.*)")
+        df["seed"] = seed_df
+
+        df["exps"] = df["filenames"].str.extract(r"(should.*)_seed")
+        df = df.drop("filenames", axis=1)
+
+        df = df.groupby(["exps"]).agg({"accuracies": ["mean", "min", "max", "std"]})
+        print(df)
