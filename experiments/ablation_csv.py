1cb375022611eaa3c3fbc954b28aeb7f5725d57e
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-08-22 14:33:39 -0400

    Refactored experiments

--- experiments/ablation_csv.py
+++ experiments/ablation_csv.py
@@ -16,21 +16,4 @@ if __name__ == "__main__":
 
     for df_key in dfs.keys():
         print(df_key)
-
-        df = dfs[df_key]
-        df.index.name = "run_name"
-
-        df["accuracy"] = df[1]
-        df = df.drop(1, axis=1)
-        df["filenames"] = df.index
-        df.index = range(0, len(df))
-
-        seed_df = df["filenames"].str.extract(r"seed=(?P<seed>.*)")
-        df["seed"] = seed_df
-
-        df["exps"] = df["filenames"].str.extract(r"(should.*)_seed")
-        df = df.drop("filenames", axis=1)
-
-        df = df.groupby(["exps"]).agg({"accuracy": ["mean", "min", "max", "std"]})
-
-        df.to_csv(out_path)
+        print(dfs.get(df_key))
