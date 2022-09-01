4a1b7590cae0bd4f56f2fe23e8fb5e90d4d8cc37
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-01 18:25:28 -0400

    Fixed paths for experiments

--- experiments/particular_exp.py
+++ experiments/particular_exp.py
@@ -5,12 +5,12 @@ from utils import create_shell_files, populate_default_scripts
 if __name__ == "__main__":
     hosts = np.array([1, 2, 3, 4, 5, 6])
     all_scripts = [
-        "python3 main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 main.py --algorithm fedvae --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
-        "python3 main.py --algorithm fedvae --dataset cifar10 --num_users 10 --alpha 0.01 --saffmple_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
+        "python3 ../main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
+        "python3 ../main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
+        "python3 ../main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
+        "python3 ../main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
+        "python3 ../main.py --algorithm fedvae --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
+        "python3 ../main.py --algorithm fedvae --dataset cifar10 --num_users 10 --alpha 0.01 --saffmple_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
     ]
 
     print("Number of experiments:", len(all_scripts))
