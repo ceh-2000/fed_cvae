7776a392931aab00d40619f1d676ce5afe35f7f5
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-01 19:15:55 -0400

    Fixed typo sad sad

--- experiments/particular_exp.py
+++ experiments/particular_exp.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         "python3 ../main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
         "python3 ../main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
         "python3 ../main.py --algorithm fedvae --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
-        "python3 ../main.py --algorithm fedvae --dataset cifar10 --num_users 10 --alpha 0.01 --saffmple_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
+        "python3 ../main.py --algorithm fedvae --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
     ]
 
     print("Number of experiments:", len(all_scripts))
