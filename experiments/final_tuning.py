055eb06bea9555356a6be1d4901ed2aea5923168
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-01 23:21:31 -0400

    New hyperparameter tuning fedvae

--- experiments/final_tuning.py
+++ experiments/final_tuning.py
@@ -4,7 +4,7 @@ from experiments.utils import create_shell_files
 
 if __name__ == "__main__":
     hosts = np.array(
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
     )
 
     all_scripts = []
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     algorithm = "fedvae"
     num_users = 10
     glob_epochs = 1
-    local_epochs = 15
     alpha = 0.01
     sample_ratio = 0.5
     beta = 1.0
@@ -23,18 +22,19 @@ if __name__ == "__main__":
     should_log = 1
     local_LR = 0.001
     decoder_LR = 0.01
+    should_weight = 1
 
-    dataset_vals = ["mnist", "fashion"]
-    z_dim_vals = [10, 50, 100]
-    decoder_num_epochs_vals = [3, 5, 7]
+    dataset_vals = ["svhn", "cifar10"]
+    z_dim_vals = [50, 100, 250, 500]
+    decoder_num_epochs_vals = [5, 10, 15]
     classifier_num_epochs_vals = [5, 10, 15]
-    should_weight_vals = [0, 1]
+    local_epochs_vals = [15, 20, 25, 30]
 
     for dataset in dataset_vals:
         for z_dim in z_dim_vals:
             for decoder_num_epochs in decoder_num_epochs_vals:
                 for classifier_num_epochs in classifier_num_epochs_vals:
-                    for should_weight in should_weight_vals:
+                    for local_epochs in local_epochs_vals:
                         all_scripts.append(
                             f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} "
                             f"--algorithm {algorithm} --dataset {dataset} --num_users {num_users} --alpha {alpha} "
@@ -46,80 +46,80 @@ if __name__ == "__main__":
                             f"--local_LR {local_LR} --decoder_LR {decoder_LR} --should_weight {should_weight}"
                         )
 
-    # One-shot FL tuning
-    algorithm = "oneshot"
-    num_users = 10
-    glob_epochs = 1
-    alpha = 0.01
-    sample_ratio = 0.5
-    user_data_split = 0.8
-    use_adam = 1
-    should_log = 1
-
-    dataset_vals = ["mnist", "fashion"]
-    local_epochs_vals = [5, 10, 15]
-    local_LR_vals = [0.01, 0.001, 0.0001]
-    K_vals = [3, 5, 7]
-
-    # one_shot_sampling_vals = ["random", "data", "validation", "all"]
-    one_shot_sampling_vals = ["random", "data"]
-    for dataset in dataset_vals:
-        for local_epochs in local_epochs_vals:
-            for local_LR in local_LR_vals:
-                for one_shot_sampling in one_shot_sampling_vals:
-                    for K in K_vals:
-                        all_scripts.append(
-                            f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
-                            f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} "
-                            f"--alpha {alpha} --sample_ratio {sample_ratio} --dataset {dataset} --local_LR {local_LR} "
-                            f"--one_shot_sampling {one_shot_sampling} --K {K}"
-                        )
-
-    one_shot_sampling = "validation"
-    for dataset in dataset_vals:
-        for local_epochs in local_epochs_vals:
-            for local_LR in local_LR_vals:
-                for K in K_vals:
-                    all_scripts.append(
-                        f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
-                        f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} --alpha {alpha} "
-                        f"--sample_ratio {sample_ratio} --one_shot_sampling {one_shot_sampling} "
-                        f"--user_data_split {user_data_split} --dataset {dataset} --local_LR {local_LR} --K {K}"
-                    )
-
-    one_shot_sampling = "all"
-    for dataset in dataset_vals:
-        for local_epochs in local_epochs_vals:
-            for local_LR in local_LR_vals:
-                all_scripts.append(
-                    f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
-                    f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} --alpha {alpha} "
-                    f"--sample_ratio {sample_ratio} --one_shot_sampling {one_shot_sampling} --dataset {dataset} "
-                    f"--local_LR {local_LR}"
-                )
-
-    # FedAvg Tuning
-    algorithm = "fedavg"
-    num_users = 10
-    glob_epochs = 1
-    alpha = 0.01
-    sample_ratio = 0.5
-    use_adam = 1
-    should_log = 1
-
-    dataset_vals = ["mnist", "fashion"]
-    local_LR_vals = [0.01, 0.001, 0.0001]
-    local_epochs_vals = [5, 7, 10]
-
-    for dataset in dataset_vals:
-        for local_LR in local_LR_vals:
-            for local_epochs in local_epochs_vals:
-                all_scripts.append(
-                    f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
-                    f"--num_users {num_users} --glob_epochs {glob_epochs} --alpha {alpha} "
-                    f"--sample_ratio {sample_ratio} --dataset {dataset} --local_LR {local_LR} "
-                    f"--local_epochs {local_epochs}"
-                )
+    # # One-shot FL tuning
+    # algorithm = "oneshot"
+    # num_users = 10
+    # glob_epochs = 1
+    # alpha = 0.01
+    # sample_ratio = 0.5
+    # user_data_split = 0.8
+    # use_adam = 1
+    # should_log = 1
+    #
+    # dataset_vals = ["mnist", "fashion"]
+    # local_epochs_vals = [5, 10, 15]
+    # local_LR_vals = [0.01, 0.001, 0.0001]
+    # K_vals = [3, 5, 7]
+    #
+    # # one_shot_sampling_vals = ["random", "data", "validation", "all"]
+    # one_shot_sampling_vals = ["random", "data"]
+    # for dataset in dataset_vals:
+    #     for local_epochs in local_epochs_vals:
+    #         for local_LR in local_LR_vals:
+    #             for one_shot_sampling in one_shot_sampling_vals:
+    #                 for K in K_vals:
+    #                     all_scripts.append(
+    #                         f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
+    #                         f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} "
+    #                         f"--alpha {alpha} --sample_ratio {sample_ratio} --dataset {dataset} --local_LR {local_LR} "
+    #                         f"--one_shot_sampling {one_shot_sampling} --K {K}"
+    #                     )
+    #
+    # one_shot_sampling = "validation"
+    # for dataset in dataset_vals:
+    #     for local_epochs in local_epochs_vals:
+    #         for local_LR in local_LR_vals:
+    #             for K in K_vals:
+    #                 all_scripts.append(
+    #                     f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
+    #                     f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} --alpha {alpha} "
+    #                     f"--sample_ratio {sample_ratio} --one_shot_sampling {one_shot_sampling} "
+    #                     f"--user_data_split {user_data_split} --dataset {dataset} --local_LR {local_LR} --K {K}"
+    #                 )
+    #
+    # one_shot_sampling = "all"
+    # for dataset in dataset_vals:
+    #     for local_epochs in local_epochs_vals:
+    #         for local_LR in local_LR_vals:
+    #             all_scripts.append(
+    #                 f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
+    #                 f"--num_users {num_users} --glob_epochs {glob_epochs} --local_epochs {local_epochs} --alpha {alpha} "
+    #                 f"--sample_ratio {sample_ratio} --one_shot_sampling {one_shot_sampling} --dataset {dataset} "
+    #                 f"--local_LR {local_LR}"
+    #             )
+    #
+    # # FedAvg Tuning
+    # algorithm = "fedavg"
+    # num_users = 10
+    # glob_epochs = 1
+    # alpha = 0.01
+    # sample_ratio = 0.5
+    # use_adam = 1
+    # should_log = 1
+    #
+    # dataset_vals = ["mnist", "fashion"]
+    # local_LR_vals = [0.01, 0.001, 0.0001]
+    # local_epochs_vals = [5, 7, 10]
+    #
+    # for dataset in dataset_vals:
+    #     for local_LR in local_LR_vals:
+    #         for local_epochs in local_epochs_vals:
+    #             all_scripts.append(
+    #                 f"python3 ../main.py --should_log {should_log} --use_adam {use_adam} --algorithm {algorithm} "
+    #                 f"--num_users {num_users} --glob_epochs {glob_epochs} --alpha {alpha} "
+    #                 f"--sample_ratio {sample_ratio} --dataset {dataset} --local_LR {local_LR} "
+    #                 f"--local_epochs {local_epochs}"
+    #             )
 
     print("Number of experiments:", len(all_scripts))
 
