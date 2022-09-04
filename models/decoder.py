0b1c8d62d9b29e2384b1024fef07671f4c07f918
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-03 22:16:06 -0400

    Added support for ResNet-based CVAE's

--- models/decoder.py
+++ models/decoder.py
@@ -74,12 +74,88 @@ class ConditionalDecoderAlt(nn.Module):
         return self.model(concat_vec)
 
 
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, upsample):
+        super().__init__()
+        if upsample:
+            self.conv1 = nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size=3, stride=2, padding=1
+            )
+            self.shortcut = nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=1, stride=2),
+                nn.BatchNorm2d(out_channels),
+            )
+        else:
+            self.conv1 = nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size=3, stride=1, padding=1
+            )
+            self.shortcut = nn.Sequential()
+
+        self.conv2 = nn.ConvTranspose2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, X):
+        shortcut = self.shortcut(X)
+        X = nn.ReLU()(self.bn1(self.conv1(X)))
+        X = nn.ReLU()(self.bn2(self.conv2(X)))
+        X = X + shortcut
+        return nn.ReLU()(X)
+
+
+class ConditionalDecoderResNet(nn.Module):
+    """Decoder CNN with ResNet structure"""
+
+    def __init__(self, num_classes, num_channels, z_dim):
+        super().__init__()
+
+        self.layer_final_reverse = nn.Sequential(
+            nn.Linear(z_dim + num_classes, 1024),
+            View((-1, 256, 2, 2)),
+            nn.ReLU(True),
+        )
+
+        self.layer3_reverse = nn.Sequential(
+            ResBlock(256, 256, upsample=False),
+            ResBlock(256, 128, upsample=True),
+            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=1, padding=1),
+        )
+
+        self.layer2_reverse = nn.Sequential(
+            ResBlock(128, 128, upsample=False),
+            ResBlock(128, 64, upsample=True),
+            nn.ConvTranspose2d(64, 64, kernel_size=4, stride=1, padding=1),
+        )
+
+        self.layer1_reverse = nn.Sequential(
+            ResBlock(64, 64, upsample=False),
+            ResBlock(64, 32, upsample=True),
+            nn.ConvTranspose2d(32, 32, kernel_size=4, stride=1, padding=1),
+        )
+
+        self.layer0_reverse = nn.Sequential(
+            nn.ConvTranspose2d(32, num_channels, kernel_size=6, stride=2, padding=2),
+        )
+
+    def forward(self, z, y_hot):
+        y_hot = y_hot.to(torch.float32)
+        concat_vec = torch.cat((z, y_hot), 1)  # sampled z and a one-hot class vector
+
+        X = self.layer_final_reverse(concat_vec)
+        X = self.layer3_reverse(X)
+        X = self.layer2_reverse(X)
+        X = self.layer1_reverse(X)
+        X = self.layer0_reverse(X)
+
+        return X
+
+
 if __name__ == "__main__":
     img_size = 32
     num_classes = 10
-    num_channels = 1
+    num_channels = 3
     z_dim = 50
-    summary(
-        ConditionalDecoderAlt(img_size, num_classes, num_channels, z_dim).model,
-        (1, (z_dim + num_classes)),
-    )
+    model = ConditionalDecoderResNet(num_classes, num_channels, z_dim)
+    print(model(torch.randn((1, z_dim)), torch.randn((1, num_classes))).shape)
--- models/encoder.py
+++ models/encoder.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torchsummary import summary
 
@@ -34,7 +35,7 @@ class ConditionalEncoder(nn.Module):
 
 
 class ConditionalEncoderAlt(nn.Module):
-    """Basic CNN with a few layers different """
+    """Basic CNN with a few layers different"""
 
     def __init__(self, num_channels, image_size, z_dim):
         super().__init__()
@@ -56,36 +57,82 @@ class ConditionalEncoderAlt(nn.Module):
 
     def forward(self, X):
         return self.model(X)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample):
+        super().__init__()
+        if downsample:
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=2, padding=1
+            )
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
+                nn.BatchNorm2d(out_channels),
+            )
+        else:
+            self.conv1 = nn.Conv2d(
+                in_channels, out_channels, kernel_size=3, stride=1, padding=1
+            )
+            self.shortcut = nn.Sequential()
+
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, X):
+        shortcut = self.shortcut(X)
+        X = nn.ReLU()(self.bn1(self.conv1(X)))
+        X = nn.ReLU()(self.bn2(self.conv2(X)))
+        X = X + shortcut
+        return nn.ReLU()(X)
+
 
 class ConditionalEncoderResNet(nn.Module):
     """CNN with ResNet structure"""
 
-    def __init__(self, num_channels, image_size, z_dim):
+    def __init__(self, num_channels, z_dim):
         super().__init__()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(num_channels, 32, 4, 2, 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-            nn.Conv2d(32, 64, 8, 2, 1),
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            nn.Conv2d(64, 64, 4, 3, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            nn.Flatten(start_dim=1),
-            nn.Linear(int(image_size / 2) * int(image_size / 2), 300),
-            nn.Linear(300, z_dim * 2),
+            nn.ReLU(),
         )
 
+        self.layer1 = nn.Sequential(
+            ResBlock(64, 64, downsample=False), ResBlock(64, 64, downsample=False)
+        )
+
+        self.layer2 = nn.Sequential(
+            ResBlock(64, 128, downsample=True), ResBlock(128, 128, downsample=False)
+        )
+
+        self.layer3 = nn.Sequential(
+            ResBlock(128, 256, downsample=True), ResBlock(256, 256, downsample=False)
+        )
+
+        # Final "layer"
+        self.fc = nn.Linear(1024, z_dim * 2)
+
     def forward(self, X):
-        return self.model(X)
+        X = self.layer0(X)
+        X = self.layer1(X)
+        X = self.layer2(X)
+        X = self.layer3(X)
+        X = nn.Flatten()(X)
+        X = self.fc(X)
+
+        return X
+
 
 if __name__ == "__main__":
     img_size = 32
-    num_channels = 1
+    num_channels = 3
     z_dim = 50
-    summary(
-        ConditionalEncoderAlt(num_channels, img_size, z_dim).model,
-        (num_channels, img_size, img_size),
-    )
+    model = ConditionalEncoderResNet(num_channels, z_dim)
+    # summary(model, (num_channels, img_size, img_size))
+    print(model(torch.randn((3, num_channels, img_size, img_size))).shape)
