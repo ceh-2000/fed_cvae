429568de9162ac369a20a8a5c5e37f014f4a1399
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-06 00:51:57 -0400

    Wrote experiments to test out fedvae with svhn, transforms, and more local training

--- experiments/particular_exp.py
+++ experiments/particular_exp.py
@@ -3,15 +3,29 @@ import numpy as np
 from utils import create_shell_files, populate_default_scripts
 
 if __name__ == "__main__":
-    hosts = np.array([1, 2, 3, 4, 5, 6])
-    all_scripts = [
-        "python3 ../main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 ../main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 100.0 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 ../main.py --algorithm fedavg --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 ../main.py --algorithm fedavg --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 10 --should_log 1 --local_LR 0.001 --use_adam 1",
-        "python3 ../main.py --algorithm fedvae --dataset svhn --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
-        "python3 ../main.py --algorithm fedvae --dataset cifar10 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1 --local_epochs 15 --local_LR 0.001 --should_log 1 --z_dim 500 --beta 1.0 --classifier_num_train_samples 5000 --classifier_epochs 5 --decoder_num_train_samples 5000 --decoder_epochs 10 --decoder_LR 0.01 --use_adam 1",
-    ]
+    hosts = np.array([1])
+    all_scripts = []
+    datasets = ["svhn"]
+    algorithms = ["fedvae"]
+    default_script = f"python3 ../main.py --should_log 1 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1"
+    default_scripts_dict = populate_default_scripts(
+        datasets, algorithms, default_script
+    )
+
+    experiments = ["--transform_exp=0", "--transform_exp=1"]
+
+    all_seeds = [1588, 1693, 7089]
+
+    for seed in all_seeds:
+        for exp in experiments:
+            for default_script_name in default_scripts_dict:
+                cur_script = default_scripts_dict.get(default_script_name)
+                cur_name = (
+                    f"runs/{default_script_name}_{exp.replace('--', '')}_seed={seed}"
+                )
+                all_scripts.append(
+                    f"{cur_script} {exp.replace('=', ' ')} --seed {seed} --cur_run_name {cur_name}"
+                )
 
     print("Number of experiments:", len(all_scripts))
 
