38fa3439e44fe1d240c82242a3ea7cb4d8f8aca5
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-17 13:25:06 -0400

    User experiments with seeded data splits for mnist and fashion all algorithms

--- experiments/num_users.py
+++ experiments/num_users.py
@@ -5,25 +5,27 @@ from utils import create_shell_files, populate_default_scripts
 if __name__ == "__main__":
     hosts = np.array([19, 20, 21, 22, 23, 24])
     all_scripts = []
-    datasets = ["svhn"]
+    datasets = ["mnist", "fashion"]
     algorithms = ["fedvae", "onefedvae", "fedavg", "oneshot"]
-    default_script = f"python3 ../main.py --should_log 1 --sample_ratio 1.0 --glob_epochs 1 --alpha 0.01"
+    default_script = f"python3 ../main.py --should_log 1 --sample_ratio 0.5 --glob_epochs 1 --alpha 0.01"
 
     default_scripts_dict = populate_default_scripts(
         datasets, algorithms, default_script
     )
 
     num_users = [5, 10, 20, 50]
-    all_seeds = [1588, 1693, 7089, 4488, 3776]
+    all_seeds = [1693]
+    all_data_seeds = [1588, 1693, 7089, 4488, 3776]
 
     for seed in all_seeds:
-        for u in num_users:
-            for default_script_name in default_scripts_dict.keys():
-                cur_name = f"runs/{default_script_name}_num_users={u}_seed={seed}"
-                script = default_scripts_dict.get(default_script_name)
-                all_scripts.append(
-                    f"{script} --num_users {u} --seed {seed} --cur_run_name {cur_name}"
-                )
+        for data_seed in all_data_seeds:
+            for u in num_users:
+                for default_script_name in default_scripts_dict.keys():
+                    cur_name = f"runs/{default_script_name}_num_users={u}_data_seed={data_seed}"
+                    script = default_scripts_dict.get(default_script_name)
+                    all_scripts.append(
+                        f"{script} --num_users {u} --seed {seed} --data_seed {data_seed} --cur_run_name {cur_name}"
+                    )
 
     print("Number of experiments:", len(all_scripts))
 
