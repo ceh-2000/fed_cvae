91c93c5675710da8e123742bc85163b547ae01ad
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-17 14:05:48 -0400

    Enabled GPUs for centralized model training wrote experiment training script for centralized models

--- experiments/heterogeneous_models.py
+++ experiments/heterogeneous_models.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     all_scripts = []
     datasets = ["mnist", "fashion"]
     algorithms = ["fedvae", "onefedvae"]
-    default_script = f"python3 ../main.py --should_log 1 --num_users 10 --alpha 0.01 --sample_ratio 0.5 --glob_epochs 1"
+    default_script = (
+        f"python3 ../main.py --should_log 1 --num_users 10 --alpha 0.01 --glob_epochs 1"
+    )
     default_scripts_dict = populate_default_scripts(
         datasets, algorithms, default_script
     )
