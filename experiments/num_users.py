7c752ee4693f958d318d1e8ed75f68cea96b24f2
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-09-17 13:39:56 -0400

    Updated hyperparameter files for svhn and new num_users experiments

--- experiments/num_users.py
+++ experiments/num_users.py
@@ -5,9 +5,9 @@ from utils import create_shell_files, populate_default_scripts
 if __name__ == "__main__":
     hosts = np.array([19, 20, 21, 22, 23, 24])
     all_scripts = []
-    datasets = ["mnist", "fashion"]
+    datasets = ["svhn"]
     algorithms = ["fedvae", "onefedvae", "fedavg", "oneshot"]
-    default_script = f"python3 ../main.py --should_log 1 --sample_ratio 0.5 --glob_epochs 1 --alpha 0.01"
+    default_script = f"python3 ../main.py --should_log 1 --glob_epochs 1 --alpha 0.01"
 
     default_scripts_dict = populate_default_scripts(
         datasets, algorithms, default_script
