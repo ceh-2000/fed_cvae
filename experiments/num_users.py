e1b7cd94c08f0bc9f183679e42c4de850d39025d
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2022-11-13 01:15:04 -0500

    New experiments for tonight

--- experiments/num_users.py
+++ experiments/num_users.py
@@ -3,7 +3,7 @@ import numpy as np
 from utils import create_shell_files, populate_default_scripts
 
 if __name__ == "__main__":
-    hosts = np.array([19, 20, 21, 22, 23, 24])
+    hosts = np.array([7, 8, 10, 12])
     all_scripts = []
     datasets = ["mnist", "fashion", "svhn"]
     algorithms = ["fedavg", "oneshot", "fedvae", "onefedvae"]
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     )
 
     num_users = [5, 10, 20, 50]
-    all_seeds = [1588, 1693, 7089, 4488, 3776]
-    all_data_seeds = [1693]
+    all_seeds = [1693]
+    all_data_seeds = [1588, 1693, 7089, 4488, 3776]
 
     for seed in all_seeds:
         for data_seed in all_data_seeds:
             for u in num_users:
                 for default_script_name in default_scripts_dict.keys():
-                    cur_name = f"runs/{default_script_name}_num_users={u}_seed={seed}"
+                    cur_name = f"runs/{default_script_name}_num_users={u}_data_seed={data_seed}"
                     script = default_scripts_dict.get(default_script_name)
                     all_scripts.append(
                         f"{script} --num_users {u} --seed {seed} --data_seed {data_seed} --cur_run_name {cur_name}"
@@ -29,4 +29,4 @@ if __name__ == "__main__":
 
     print("Number of experiments:", len(all_scripts))
 
-    create_shell_files(all_scripts, hosts, "num_users")
+    create_shell_files(all_scripts, hosts, "num_users_data")
